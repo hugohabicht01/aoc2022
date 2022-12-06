636a40f4d5e0f0e73945b4bf41546c500296073c
hugohabicht01 <anon@example.org>
2022-12-06 09:33:41 +0000

    day 06 part 1

---
+++ 06/main.ts
@@ -0,0 +1,55 @@
+import { example, input } from './input'
+
+class FixedLengthQueue<T> {
+  private items: T[]
+  private maxLength: number
+
+  constructor(maxLength: number) {
+    this.items = []
+    this.maxLength = maxLength
+  }
+
+  public enqueue(item: T) {
+    if (this.items.length >= this.maxLength)
+      this.items.shift()
+
+    this.items.push(item)
+  }
+
+  public dequeue() {
+    return this.items.shift()
+  }
+
+  public peek() {
+    return this.items[0]
+  }
+
+  public get length() {
+    return this.items.length
+  }
+
+  public itemsAllUnique() {
+    return (new Set(this.items)).size === this.items.length && this.items.length === this.maxLength
+  }
+}
+
+function isLowercaseSingleCharType(value: any): value is LowercaseSingleCharType {
+  return /^[a-z]$/.test(value)
+}
+
+type LowercaseSingleCharType = 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g' | 'h' | 'i' | 'j' | 'k' | 'l' | 'm' | 'n' | 'o' | 'p' | 'q' | 'r' | 's' | 't' | 'u' | 'v' | 'w' | 'x' | 'y' | 'z'
+
+const parse = (databuffer: string): LowercaseSingleCharType[] => databuffer.split('').filter(isLowercaseSingleCharType)
+
+const part1 = (chars: LowercaseSingleCharType[]) => {
+  const q = new FixedLengthQueue<LowercaseSingleCharType>(4)
+  let count = 0
+  for (const c of chars) {
+    count++
+    q.enqueue(c)
+    if (q.itemsAllUnique())
+      return count
+  }
+}
+
+console.log(part1(parse(input)))
